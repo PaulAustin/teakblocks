ae684f068f831dcffc3f9cd58ceb06f607461e14
PaulAustin <canoetime@gmail.com>
2016-08-22 10:28:23 -0500

    First step in module format
    Moving things from global name space, possibly for browserify.

---
+++ design/log.md
@@ -0,0 +1,29 @@
+August 19, 2016
+Met with Sidharth & Rohit, helped merge in object cloning routine
+for palette behaviours.
+
+August 20, 2016
+SVG path experiments. UI for loops, and branching
+
+Considering browserify
+
+August 21, 2016
+
+Morning thought - A good UX is like a smooth walking pace or even a dance
+dare I say the best will let you run like the wind.
+
+The history of computer UX, is filled with system that work as well as steady
+state robots that can pause at any time. Their gate is awkward and slow.
+The truth is, to walk, dance or run the system is never steady, it is always
+transitioning, technically it is falling but that sounds odd, from state to
+state. A marriage of intent with gravity and inertia. A good UX is the same
+a learnable fluid physics that the human mind will enjoy.
+
+gestures
+
+- *Drag* sliding the object from start to finish
+  *Fling* momentary sliding of the object that end with enogh speed that the
+  object has inertia
+
+  *Flick* shorter than an fling, link a thump or pop. The intent is not about
+  where you want the object to end up, other than you want it done.
--- teak/teakblocks.js
+++ teak/teakblocks.js
@@ -19,77 +19,79 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-
-function initTeakBlockEditor(svg, text) {
-    // TODO: move editor from global to constructed object.
-    // TODO have caller add items to palette.
-    editor.init(svg, text);
-    return editor;
-}
-
-editor = {
-  init: function (svg, text) {
-    this.blocks = [];
-    this.palette = [];
-    this.svg = svg;
-    this.teakCode = text;
-    initInteactJS();
-    initPalettes();
-  },
-
-  elementToBlock: function(el) {
-      var text = el.getAttribute('block-id');
-      values = text.split(':');
-      if (values[0] === 'e') {
-        return this.blocks[values[1]];
-      } else if (values[0] === 'p') {
-        return this.palette[values[1]];
-      } else {
-        return null;
-      }
-  },
-  addBlock: function(x, y, name, params) {
-     var block = new FunctionBlock(x, y, name, 'e:' + editor.blocks.length);
-     block.params = params;
-     block.paletteBlock = false;
-     editor.blocks.push(block);
-  },
-  addPaletteBlock: function(x, y, name, params) {
-     var block = new FunctionBlock(x, y, name, 'p:' + editor.palette.length);
-     block.params = params;
-     block.paletteBlock = true;
-     editor.palette.push(block);
-  },
-  popPaletteItem: function(block, y, x, name, id, newId){
-      var index = editor.palette.indexOf(block); 
-    if (index !== -1) {
-
-        editor.palette[index] = new FunctionBlock(x, y, name, id);
-        editor.palette[index].paletteBlock = true;
-    }
-    block.paletteBlock = false;
-    block.setBlockAttribute(block);
-    editor.blocks.push(block);
-  }
-
-};
-
-function dmoveRect(rect, dx, dy) {
+/*
+dmoveRect = function dmoveRect(rect, dx, dy) {
   rect.left += dx;
   rect.top += dy;
   rect.right += dx;
   rect.bottom += dy;
-}
+};
+*/
+module.tbe = {};
+var tbe = module.tbe;
+
+tbe.svgns = 'http://www.w3.org/2000/svg';
+
+tbe.blocks = [];
+tbe.palette = [];
+
+tbe.init = function init(svg, text) {
+  this.svg = svg;
+  this.teakCode = text;
+  this.initInteactJS();
+  interact.maxInteractions(Infinity);
+  return this;
+};
+
+tbe.elementToBlock = function(el) {
+    var text = el.getAttribute('block-id');
+    if (text === null)
+      return null;
+    values = text.split(':');
+    if (values[0] === 'e') {
+      return this.blocks[values[1]];
+    } else if (values[0] === 'p') {
+      return this.palette[values[1]];
+    } else {
+      return null;
+    }
+};
+
+tbe.addBlock = function(x, y, name, params) {
+   var block = new this.unctionBlock(x, y, name, 'e:' + this.blocks.length);
+   block.params = params;
+   block.paletteBlock = false;
+   this.blocks.push(block);
+};
+
+tbe.addPaletteBlock = function(x, y, name, params) {
+   var block = new this.FunctionBlock(x, y, name, 'p:' + this.palette.length);
+   block.params = params;
+   block.paletteBlock = true;
+   this.palette.push(block);
+};
+
+tbe.popPaletteItem = function(block, y, x, name, id, newId){
+  var index = this.palette.indexOf(block);
+  if (index !== -1) {
+
+      this.palette[index] = new this.FunctionBlock(x, y, name, id);
+      this.palette[index].paletteBlock = true;
+  }
+  block.paletteBlock = false;
+  block.setBlockAttribute(block);
+  this.blocks.push(block);
+};
 
 //TODO change this to a left, right, over, nowhere-near reply
-function intersectRect(dragRect, r2) {
+tbe.inSnapRegion = function inSnapRegion(dragRect, r2) {
   return !(r2.left > dragRect.right ||
            r2.right < dragRect.left - 50 ||
            r2.top > (dragRect.bottom - 10) ||
            r2.bottom < (dragRect.top + 10));
- }
+ };
 
-function FunctionBlock (x, y, blockName, svg_id) {
+tbe.FunctionBlock = function FunctionBlock (x, y, blockName, svg_id) {
   // Make an JS object that wraps an SVG object
   this.rect  = {
       left:   0,
@@ -113,37 +115,38 @@ function FunctionBlock (x, y, blockName, svg_id) {
 
   this.id = svg_id;
 
-  var group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+  var group = document.createElementNS(svgns, 'g');
   group.setAttribute('class', 'function-block');
 
   // Create the actual SVG object. Its a group of two pieces
   // a rounded rect and a text box. The group is moved by changing
   // it's transform (see dmove)
 
-  var rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+  var rect = document.createElementNS(svgns, 'rect');
   rect.setAttribute('class', 'function-block');
   // For safari 8/14/2016 rx or ry must be explicitly set other wise rx/ry
   // values in css will be ignored. Perhasp a more optimized rect is used.
   rect.setAttribute('rx', 1);
   rect.setAttribute('block-id', svg_id);
 
-  var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+  var text = document.createElementNS(svgns, 'text');
   text.setAttribute('class', 'function-text');
   text.setAttribute('x', '10');
   text.setAttribute('y', '45');
   text.textContent = blockName;
 
   group.appendChild(rect);
+  // group.appendChild(createBranchPath());
   group.appendChild(text);
 
   this.el = group;
   this.rrect= rect;
 
   this.dmove(x, y, true);
-  editor.svg.appendChild(group);
-}
+  tbe.svg.appendChild(group);
+};
 
-FunctionBlock.prototype.setDraggingState = function (state) {
+tbe.FunctionBlock.prototype.setDraggingState = function (state) {
   // If this block is in a chain, disconnect it from blocks in front.
   if (state && (this.prev !== null)) {
     this.prev.next = null;
@@ -159,17 +162,19 @@ FunctionBlock.prototype.setDraggingState = function (state) {
   }
 };
 
-FunctionBlock.prototype.setBlockAttribute = function(event){
-  event.id = 'e:' + editor.blocks.length;
-  event.rrect.setAttribute('block-id', 'e:' + editor.blocks.length);
+tbe.FunctionBlock.prototype.setBlockAttribute = function(event){
+  event.id = 'e:' + tbe.blocks.length;
+  event.rrect.setAttribute('block-id', 'e:' + tbe.blocks.length);
 };
 
-
-FunctionBlock.prototype.dmove = function (dx, dy, snapToInt) {
+tbe.FunctionBlock.prototype.dmove = function (dx, dy, snapToInt) {
   var block = this;
   while (block !== null) {
     var r = block.rect;
-    dmoveRect(r, dx, dy);
+    r.left += dx;
+    r.top += dy;
+    r.right += dx;
+    r.bottom += dy;
     if (snapToInt) {
       r.top = Math.round(r.top);
       r.left = Math.round(r.left);
@@ -181,23 +186,23 @@ FunctionBlock.prototype.dmove = function (dx, dy, snapToInt) {
   }
 };
 
-FunctionBlock.prototype.hilite = function(state) {
+tbe.FunctionBlock.prototype.hilite = function(state) {
   if (state) {
     // bring hilite block to top. block dont normally overlap
     // but ones that are being dragged need to.
-    editor.svg.appendChild(this.el);
+    tbe.svg.appendChild(this.el);
     this.rrect.setAttribute('class', 'function-block-dragging');
   } else {
     this.rrect.setAttribute('class', 'function-block');
   }
 };
 
-FunctionBlock.prototype.hilitePossibleTarget = function() {
+tbe.FunctionBlock.prototype.hilitePossibleTarget = function() {
   var thisBlock = this;
   var target = null;
-  editor.blocks.forEach(function(entry) {
+  tbe.blocks.forEach(function(entry) {
     if (entry !== thisBlock  && !entry.dragging && (entry.next === null)) {
-      if (intersectRect(thisBlock.rect, entry.rect)) {
+      if (tbe.inSnapRegion(thisBlock.rect, entry.rect)) {
         target = entry;
       }
     }
@@ -216,24 +221,24 @@ FunctionBlock.prototype.hilitePossibleTarget = function() {
 };
 
 // Show the socket this block will be put in when dragging stops.
-FunctionBlock.prototype.insertTargetShadows = function(target) {
+tbe.FunctionBlock.prototype.insertTargetShadows = function(target) {
   var block = this;
   var x = target.rect.right;
   var shadow = null;
   while (block !== null) {
-    shadow = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    shadow = document.createElementNS(svgns, 'rect');
     shadow.setAttribute('class', 'shadow-block');
     shadow.setAttribute('rx', 1);
     shadow.style.x = x;
     shadow.style.y = target.rect.top;
-    editor.svg.insertBefore(shadow, editor.svg.firstChild);
+    tbe.svg.insertBefore(shadow, tbe.svg.firstChild);
     block.targetShadow = shadow;
     x += 80;
     block = block.next;
   }
 };
 
-FunctionBlock.prototype.removeTargetShadows = function() {
+tbe.FunctionBlock.prototype.removeTargetShadows = function() {
   var block = this;
   var shadowsToRemove = [];
   while (block !== null) {
@@ -241,7 +246,6 @@ FunctionBlock.prototype.removeTargetShadows = function() {
     if (shadow !== null) {
       shadowsToRemove.push(shadow);
       shadow.setAttribute('class', 'shadow-block-leave');
-      // editor.svg.removeChild(block.targetShadow);
       block.targetShadow = null;
     }
     block = block.next;
@@ -249,13 +253,13 @@ FunctionBlock.prototype.removeTargetShadows = function() {
   // Give some time for the animation to complete, then remove.
   setTimeout(function() {
     shadowsToRemove.forEach( function(elt) {
-      editor.svg.removeChild(elt);
+      tbe.svg.removeChild(elt);
       });
     },
     1000);
 };
 
-FunctionBlock.prototype.moveToPossibleTarget = function() {
+tbe.FunctionBlock.prototype.moveToPossibleTarget = function() {
   if (this.snapTarget !==  null && this.targetShadow !== null) {
     // Insert the block in the list
     if(true /* after */) {
@@ -279,14 +283,14 @@ FunctionBlock.prototype.moveToPossibleTarget = function() {
       ady: dy / frameCount,
       frame: frameCount,
     };
-    easeToTarget(0, this);
+    tbe.easeToTarget(0, this);
 //    this.removeTargetShadows();
   }
   this.hilite(false);
   this.snapTarget = null;
 };
 
-function easeToTarget(timeStamp, block) {
+tbe.easeToTarget = function easeToTarget(timeStamp, block) {
   var frame = block.animateState.frame;
   block.dmove(block.animateState.adx, block.animateState.ady, (frame === 1));
   if (frame > 1) {
@@ -296,31 +300,33 @@ function easeToTarget(timeStamp, block) {
     // Once animation is over shadows are covered, remove them.
     block.removeTargetShadows();
   }
-}
+};
 
 // Attach these interactions properties based on the class property of the DOM elements
-function initInteactJS() {
+tbe.initInteactJS = function initInteactJS() {
+  var thisTbe = tbe;
   interact('.function-block')
     .on('down', function (event) {
-      var block = editor.elementToBlock(event.target);
-      console.log('on down', block.name);
+      var block = thisTbe.elementToBlock(event.target);
+      if (block === null)
+        return;
       block.coasting = 0;
-    //    editor.elementToBlock(event.target).hilite(true);
     })
     .on('up', function (event) {
-        // Mark the chain as coastin. if it finds a target
-        // it will snap to it.
-        var block = editor.elementToBlock(event.target);
-        console.log('on up', block.name);
-        block.coasting = 1;
+      // Mark the chain as coastin. if it finds a target
+      // it will snap to it.
+      var block = thisTbe.elementToBlock(event.target);
+      if (block === null)
+        return;
+      block.coasting = 1;
     })
     .on('hold', function(event) {
-      var block = editor.elementToBlock(event.target);
+      var block = thisTbe.elementToBlock(event.target);
       // TODO press and hold...
     })
     .draggable({
       restrict: {
-          restriction: editor.svg,
+          restriction: thisTbe.svg,
           endOnly: true,
           // Restrictions, by default, are for the point not the whole object
           // so R and B are 1.x to inlcude the width and height of the object.
@@ -334,18 +340,22 @@ function initInteactJS() {
       },
       max: Infinity,
       onstart: function(event) {
-        var block = editor.elementToBlock(event.target);
+        var block = thisTbe.elementToBlock(event.target);
+        if (block === null)
+          return;
 
       if (block.paletteBlock) {
         id = block.id;
         id = id.split(':');
 
-        editor.popPaletteItem(block, block.rect.top, block.rect.left, block.name, block.id, 'e:' + editor.blocks.length);  
+        thisTbe.popPaletteItem(block, block.rect.top, block.rect.left, block.name, block.id, 'e:' + thisTbe.blocks.length);
       }
       block.setDraggingState(true);
       },
       onend: function(event) {
-        var block = editor.elementToBlock(event.target);
+        var block = thisTbe.elementToBlock(event.target);
+        if (block === null)
+          return;
 
         if (block.coasting > 0) {
           block.coasting = -1;
@@ -353,14 +363,16 @@ function initInteactJS() {
           block.setDraggingState(false);
         }
         // Serialize after all moving has settled. TODO clean this up.
-        setTimeout(editor.blocksToText(),500);
+        setTimeout(thisTbe.blocksToText(),500);
       },
       onmove: function (event) {
         // Since there is inertia these callbacks continue to
         // happen after the user lets go. If a target is found
         // in the coasting state, start the animation to the target.
         // dont wait to coas to a stop.
-        var block =  editor.elementToBlock(event.target);
+        var block =  thisTbe.elementToBlock(event.target);
+        if (block === null)
+          return;
 
         if (block.dragging) {
           block.dmove(event.dx, event.dy, true);
@@ -380,11 +392,11 @@ function initInteactJS() {
         }
       }
     });
-}
+};
 
-editor.blocksToText = function() {
+tbe.blocksToText = function() {
   var teakText = '(\n';
-  editor.blocks.forEach(function(entry) {
+  this.blocks.forEach(function(entry) {
     if (entry.prev === null) {
        teakText += '  (chain\n';
       var block = entry;
@@ -394,7 +406,7 @@ editor.blocksToText = function() {
           teakText += ' x:' + block.rect.left + ' y:' +  block.rect.top;
         }
         if (block.params !== null) {
-          teakText += blockParamsToText(block.params);
+          teakText += tbe.blockParamsToText(block.params);
         }
         if (block.targetShadow !== null) {
           // For debugging, this ocassionally happens since
@@ -408,27 +420,13 @@ editor.blocksToText = function() {
     }
   });
   teakText += ')\n';
-  editor.teakCode.value = teakText;
+  this.teakCode.value = teakText;
 };
 
-function blockParamsToText(params) {
+tbe.blockParamsToText = function blockParamsToText(params) {
   var text = '';
   for(var propertyName in params) {
     text += ' ' + propertyName + ':' + params[propertyName];
   }
   return text;
-}
-
-function initPalettes() {
-  interact.maxInteractions(Infinity);
-
-  editor.addPaletteBlock(400,  20, 'motor', {port:'a','power':50,'time':'2.5s'});
-  editor.addPaletteBlock(100, 120, 'wait',  {time:'2.5s'});
-  editor.addPaletteBlock(100, 220, 'light', {color:'blue'});
-  editor.addPaletteBlock(100, 320, 'sound', {note:'C5'});
-  editor.addPaletteBlock(100, 420, 'start', {when:'dio1-rising'});
-  editor.addPaletteBlock(100, 420, 'quark', {flavor:'charmed'});
-  //editor.addPaletteBlock(200, 420, 'zebra');
-
-  editor.blocksToText();
-}
+};
