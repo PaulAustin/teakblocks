02c2d0e77391f0c6fb6c185106fdefab60f2071f
PaulAustin <canoetime@gmail.com>
2016-08-25 09:11:54 -0500

    Merging Sidhnarth's palette work

--- source/index.js
+++ source/index.js
@@ -26,9 +26,13 @@ tbe.init(
   document.getElementById('editorCanvas'),
   document.getElementById('teakCode'));
 
+tbe.initPalettes();
+/*
+re work into Sidharth's code.
 tbe.addPaletteBlock(400,  20, 'motor', {port:'a','power':50,'time':'2.5s'});
 tbe.addPaletteBlock(100, 120, 'wait',  {time:'2.5s'});
 tbe.addPaletteBlock(100, 220, 'light', {color:'blue'});
 tbe.addPaletteBlock(100, 320, 'sound', {note:'C5'});
 tbe.addPaletteBlock(100, 420, 'start', {when:'dio1-rising'});
 tbe.addPaletteBlock(100, 420, 'quark', {flavor:'charmed'});
+*/
