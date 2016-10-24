e06ce099b158ece4fd413b4a3a9ce2b5cc344281
sidharths00 <sidharth@MacBook-Air.attlocal.net>
2016-10-23 20:52:03 -0500

    All blocks delete with a constant speed now
    Before, all the blocks went faster if they are farther from the center.
    Now all the blocks go the same speed regardless where on the screen
    they are

--- source/physics.js
+++ source/physics.js
@@ -1,19 +1,21 @@
 
 module.exports = function (){
 
-var d = 0;
-var n = 0;
-
+  var a = false;
 function trashBlocks(editor) {
     if (editor.diagramBlocks.length > 0 ) {
 
       editor.diagramBlocks.forEach(function(block) {
-        //while(block.rect.left > 0 || block.rect.top > 0){
-          var frameCount = 10000000000;
-
+          var frameCount = 80;
+          var xPos = block.rect.left - (window.innerWidth/2);
+          var yPos = block.rect.top - (window.innerHeight/2);
+          //need to find the hyp then divide the xPos and yPos by it
+          var hyp = Math.sqrt((xPos * xPos) + (yPos * yPos));
+          var getX = (xPos/hyp) * 10;
+          var getY = (yPos/hyp) * 10;
           block.animateState = {
-            adx: ((19 * (window.innerWidth/20)) - block.rect.left)/70,
-            ady: 0,//(window.innerHeight - block.rect.top)/frameCount,
+            adx: getX,
+            ady: getY,
             frame: frameCount,
             count: 0
           };
@@ -21,61 +23,30 @@ function trashBlocks(editor) {
           });
       }
           editor.audio.playSound(editor.audio.poof);
-          //console.log("This is list:");
-          //console.log(tbe.diagramBlocks);
-          d = new Date();
-          n = d.getTime();
+
           editor.diagramBlocks.forEach(function(block){
             dump(block, editor);
           });
-          /*tbe.diagramBlocks.forEach(function(block){
-            if(block.rect.top){
-              block.delete = true;
-            }
 
-          });*/
-
-          //
-
-          //block.dmove(0, 5, false, block);
-        //}
-
-//https://developer.mozilla.org/en-US/docs/Web/API/Detecting_device_orientation
 }
 
   function dump(block, editor) {
-  //blocks.forEach(function(block)
-    //console.log(block);
-    //console.log(block.name + " - " + block.animateState.ady);
-    //if(block.rect.top < window.innerHeight)
-    //console.log(block);
-    //block.animateState.frame = block.animateState.frame/tbe.diagramBlocks.length;
-    //console.log(block);
     if(block[0] === undefined){
         var frame = block.animateState.frame;
         block.dmove(block.animateState.adx, block.animateState.ady, (frame === 1), block);
         block.animateState.count += 1;
 
-    //console.log(block.animateState.count);
-    //console.log(block);
-    if (frame > 1 && block.rect.top < window.innerHeight) {
+    if (frame > 1 ){
       block.animateState.frame = frame - 1;
 
-      //console.log(frame);
       requestAnimationFrame(function(timestamp) {
-
-        block.animateState.ady +=  0.3;
         dump(block, editor);
       });
 
     } else {
-       // Once animation is over shadows are covered, remove them.
-       //console.log(block.animateState.ady);
-       console.log(block.animateState.ady);
-       editor.clearDiagramBlocks();
-       var d2 = new Date();
-       var n2 = d2.getTime();
-      console.log(n2 - n);
+
+        editor.clearDiagramBlocks();
+        console.log("done");
     }
   }
 }
