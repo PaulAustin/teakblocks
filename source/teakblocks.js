9db01ee24d015966911c98df4cdce249fc814b87
PaulAustin <canoetime@gmail.com>
2016-11-21 09:22:38 -0600

    Increased shadow offset
    The loop graphics and shadow blocks for insert were closely overlapping
    and hard to distinguish.

--- source/teakblocks.js
+++ source/teakblocks.js
@@ -513,8 +513,8 @@ tbe.FunctionBlock.prototype.insertTargetShadows = function(target, action) {
     // down stream blocks until the move is committed.
     // so offset them a bit.
     // TODO show abovw OR based on wherr draggin block are coming from.
-    x = target.rect.left - 15;
-    y += 25;
+    x = target.rect.left - 20;
+    y -= 25;
   } else if (action === 'append') {
     x = target.rect.right;
   } else {
