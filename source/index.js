befeb55a5904f18d67d77ac2fe83da63558a944c
sidharths00 <sidharths00@gmail.com>
2017-06-06 18:00:43 -0500

    Commented out servo and wait (for Reynosa)

--- source/index.js
+++ source/index.js
@@ -133,8 +133,8 @@ function deviceReady() {
       'sound':{},
       'motor':{},
       'twoMotor':{},
-      'servo':{},
-      'wait':{},
+      //'servo':{},
+      //'wait':{},
       'loop':{}
     }
   };
