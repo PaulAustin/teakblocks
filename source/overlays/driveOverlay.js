0c7c1daa57c64b694a498645c50a92e3d542b6eb
PaulAustin <canoetime@gmail.com>
2019-03-25 19:58:14 -0500

    Moving to SVG based drive control

--- source/overlays/driveOverlay.js
+++ source/overlays/driveOverlay.js
@@ -24,6 +24,7 @@ SOFTWARE.
 module.exports = function(){
   var log = require('./../log.js');
   var interact = require('interact.js');
+  var svgb = require('./../svgbuilder.js');
   var conductor = require('./../conductor.js');
   var overlays = require('./overlays.js');
   var dso = require('./deviceScanOverlay.js');
@@ -42,9 +43,12 @@ module.exports = function(){
     overlays.overlayDom.innerHTML = `
     <div id='overlayRoot'
      <div id='driveOverlay'>
-      <div class='slider sliderRight' data-value='0'></div>
+      <svg id='driveOverlaySvgCanvas' xmlns="http://www.w3.org/2000/svg"></svg>
+<!--      <div class='slider sliderRight' data-value='0'></div>
       <div class='slider sliderLeft' data-value='0'></div>
       <div id='drive-diagnostics' class='drive-diagnostics'>
+      -->
+
         <!--h1 class='connected-brick svg-clear'>Connected Bot: -?-</h1>
         <h1 class="drive-accelerometer svg-clear">Accelerometer: 100</h1>
         <h1 class="drive-compass svg-clear">Compass: 100</h1>
@@ -53,22 +57,83 @@ module.exports = function(){
         <h1 class="drive-encoderR svg-clear">Right Encoder: 100</h1-->
       </div>
       </div>
-    </div>
-    `;
+    </div>`
+    ;
+    window.addEventListener("resize", driveMode.onResize);
 
-    // TODO connect the stop button
-    driveMode.sliderInteract('slider');
+    driveMode.svg = document.getElementById('driveOverlaySvgCanvas');
+    driveMode.onResize();
+    driveMode.sliderInteract();
   };
 
-  driveMode.sliderInteract = function sliderInteract(eltClass) {
-    interact('.' + eltClass)              // target the matches of that selector
-      .origin('self')                     // (0, 0) will be the element's top-left
-      .restrict({drag: 'self'})           // keep the drag within the element
-      .inertia(true)                      // start inertial movement if thrown
+driveMode.onResize = function() {
+    driveMode.w = driveMode.svg.clientWidth;
+    driveMode.h = driveMode.svg.clientHeight;
+    driveMode.buildSVG();
+};
+
+  driveMode.buildSVG = function() {
+
+    var svg = driveMode.svg;
+    // Clear out the old.
+    while (svg.lastChild) {
+        svg.removeChild(svg.lastChild);
+    }
+
+    var gtop = 120;
+    var ginset = 100;
+    var gw = 120;
+    var tw = 50;
+    var gwHalf = gw / 2;
+    var gh = driveMode.h - 100 - gtop;
+
+    var lCenter = ginset + gwHalf;
+    var rCenter = driveMode.w - ginset - gwHalf;
+
+    var leftGroove = svgb.createRect('slider-groove', lCenter - gwHalf, gtop, gw, gh, gwHalf);
+    var rightGroove = svgb.createRect('slider-groove', rCenter - gwHalf, gtop, gw, gh, gwHalf);
+    driveMode.svg.appendChild(leftGroove);
+    driveMode.svg.appendChild(rightGroove);
+
+    driveMode.lThumb = svgb.createCircle('slider-thumb', lCenter, gtop, tw);
+    driveMode.rThumb = svgb.createCircle('slider-thumb', rCenter, gtop, tw);
+    driveMode.svg.appendChild(driveMode.lThumb);
+    driveMode.svg.appendChild(driveMode.rThumb);
+
+    // Move thumbs to actual value locations.
+    driveMode.moveThumbs();
+  };
+
+  driveMode.moveThumbs = function() {
+      var gtop = 120;
+      var gw = 120;
+      var gwHalf = gw / 2;
+      var gh = driveMode.h - 100 - gtop;
+
+      var lValue = -100;
+      var rValue = 100;
+
+      var range = gh - gw;
+
+      var lPx = (range * ((lValue + 100)/200));
+      var rPx = (range * ((rValue + 100)/200));
+
+      driveMode.lThumb.setAttribute('cy', lPx + gtop + gwHalf);
+      driveMode.rThumb.setAttribute('cy', rPx + gtop + gwHalf);
+  };
+
+  driveMode.sliderInteract = function() {
+
+    interact('.slider-thumb', {context:driveMode.svg})              // target the matches of that selector
+//      .origin('self')                     // (0, 0) will be the element's top-left
+//      .restrict({drag: 'self'})           // keep the drag within the element
+//      .inertia(true)                      // start inertial movement if thrown
       .draggable({                        // make the element fire drag events
         max: Infinity                     // allow drags on multiple elements
       })
       .on('dragmove', function (event) {  // call this function on every move
+          console.log("on drag move");
+          /*
         var sliderHeight = interact.getElementRect(event.target).height,
             value = event.pageY / sliderHeight;
 
@@ -80,15 +145,18 @@ module.exports = function(){
         } else if (event.target.classList.contains('sliderLeft')) {
           driveMode.displayLeft = display;
         }
+        */
       })
     .on('dragend', function(event){
+        /*
       event.target.style.paddingTop = (0.5 * 7) + 'em';
       event.target.setAttribute('data-value', 0);
       if(event.target.classList.contains('sliderRight')) {
-        driveMode.displayRight = 0;
+    //    driveMode.displayRight = 0;
       } else if (event.target.classList.contains('sliderLeft')) {
-        driveMode.displayLeft = 0;
+    //    driveMode.displayLeft = 0;
       }
+      */
     });
 
     // Allow more thatn one slide to be used at a time (multiple fingers).
@@ -97,7 +165,7 @@ module.exports = function(){
 
   driveMode.updateSlider = function() {
     var id = dso.deviceName;
-    log.trace('updTE', id);
+//    log.trace('updTE', id);
     //var changed = driveMode.displayLeft !== driveMode.pastLeft || driveMode.displayRight !== driveMode.pastRight;
     if (id !== null && id !== '-?-') {
       if (driveMode.displayLeft !== undefined && driveMode.displayLeft !== driveMode.pastLeft) {
